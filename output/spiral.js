52adfbc814ed75ddd2742234e22b7e49b6811d0d
Andrew Su <andrew.su@gmail.com>
2016-06-11 14:23:40 -0700

    nested spirals work, but need to refactor to embed coordinates into json object

--- output/spiral.js
+++ output/spiral.js
@@ -6,12 +6,12 @@ var width = 1000,    // svg width
 
 var rotation = 2 * Math.PI;   // global variable
 
-function calcCoords ( thisCenterX, thisCenterY, thisNumPoints, thisChord, thisAwayStep ) {
+function calcCoords ( thisCenterX, thisCenterY, thisNumPoints, thisChord, thisAwayStep, level ) {
   
   var coords = [];    // output array
   var theta = thisChord / thisAwayStep;
 
-  coords.push({x: thisCenterX, y: thisCenterY})
+  coords.push({x: thisCenterX, y: thisCenterY, level:level})
 
   for ( i = 0; i < thisNumPoints - 1; i++ ) {
       var away = thisAwayStep * theta;
@@ -22,24 +22,15 @@ function calcCoords ( thisCenterX, thisCenterY, thisNumPoints, thisChord, thisAw
 
       theta += thisChord / away;
     
-      coords.push({x: x, y: y});
+      coords.push({x: x, y: y, level: level});
   }
 
   return coords;
 }
 
-var svg = d3.select("#chart").append("svg")
-    .attr("width", width)
-    .attr("height", height)
-  .append("g");
+function makeSpiral ( thisCenterX, thisCenterY, thisNumPoints, thisChord, thisAwayStep, level) {
 
-var new_time;  // make this a global variable so I can inspect it
-
-function makeSpiral ( thisCenterX, thisCenterY, thisNumPoints, thisChord, thisAwayStep) {
-
-  new_time = calcCoords( thisCenterX, thisCenterY, thisNumPoints, thisChord, thisAwayStep );
-
-  console.log(new_time.length)
+  new_time = calcCoords( thisCenterX, thisCenterY, thisNumPoints, thisChord, thisAwayStep, level );
 
   var lineFunction = d3.svg.line()
                       .x(function(d) { return d.x; })
@@ -59,9 +50,40 @@ function makeSpiral ( thisCenterX, thisCenterY, thisNumPoints, thisChord, thisAw
                   .append("circle")
                   .attr("cx", function (d) { return d.x; })
                   .attr("cy", function (d) { return d.y; })
-                  .attr("r", 5);
+                  .attr("r", 5)
+                  .attr("class", function(d){ return "level".concat(d.level)});
+
+  return(new_time);
 
 }
+
+
+//makeSpiral ( centerX, centerY, numPoints, chord, awayStep );
+
+//makeSpiral ( new_time[199].x, new_time[199].y, 21, 10, 3 );
+
+function processPerson ( root, centerX, centerY, level ) {
+
+  console.log( "Processing " + root.name );
+
+  if( root.children.length == 0 ) { console.log("No children"); return; }
+
+  var coords = makeSpiral(centerX, centerY, root.children.length+1, chord, awayStep, level)
+  console.log( "COORDS.length: " + coords.length )
+  console.log( "CHILDREN.length: " + root.children.length )
+
+  for(var i=0; i<root.children.length; i++) {
+    console.log("CHILD: " + root.children[i].name)
+    processPerson( root.children[i], coords[i+1].x, coords[i+1].y, level+1)
+  }
+}
+
+var svg = d3.select("#chart").append("svg")
+    .attr("width", width)
+    .attr("height", height)
+  .append("g");
+
+var new_time;  // make this a global variable so I can inspect it
 
 var centerX = width/2,   // x coordinate of spiral center
     centerY = height/2,  // y coordinate of spiral center
@@ -69,8 +91,13 @@ var centerX = width/2,   // x coordinate of spiral center
     chord = 30,          // spacing between points
     awayStep = 10;       // rate of increase of sprial radius
 
-makeSpiral ( centerX, centerY, numPoints, chord, awayStep );
+var z; 
+d3.json("output.json", function(error, root) {
+  if (error) throw error;
 
-makeSpiral ( new_time[199].x, new_time[199].y, 21, 10, 3 );
+  z = root;   /// temporary so I can inspect it
+  console.log("ROOT: " + root);
+  processPerson( root, centerX, centerY, root.level )
+});
 
 

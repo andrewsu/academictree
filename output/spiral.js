a8bdba2754e034df1043e21f0aff04b463ac7089
Andrew Su <andrew.su@gmail.com>
2016-06-11 23:57:32 -0700

    pretty good first version

--- output/spiral.js
+++ output/spiral.js
@@ -1,58 +1,66 @@
 /// Based on code at http://bl.ocks.org/fabiovalse/dfcd8104a79aed092af1
 ///  Modified by Andrew Su on 2016-06-11 
 
-var width = 1000,    // svg width
-    height = 1000;   // svg height
+var width = 3600,    // svg width
+    height = 3600;   // svg height
 
 var rotation = 2 * Math.PI;   // global variable
 
 var spiralParams = {
   "level1": { 
     "opacity": 1,
-    "r": 10,
-    "chord": 30,
-    "awayStep": 20 
+    "r": 50,
+    "chord": 150,
+    "awayStep": 100,
+    "fontSize": 50
   },
   "level2": { 
     "opacity": 1,
-    "r": 10,
-    "chord": 15,
-    "awayStep": 10 
+    "r": 50,
+    "chord": 75,
+    "awayStep": 50,
+    "fontSize": 50
   },
   "level3": { 
     "opacity": .5,
-    "r": 5,
-    "chord": 10,
-    "awayStep": 5 
+    "r": 25,
+    "chord": 50,
+    "awayStep": 25,
+    "fontSize": 0
   },
   "level4": { 
     "opacity": .3,
-    "r": 3,
-    "chord": 5,
-    "awayStep": 5 
+    "r": 15,
+    "chord": 25,
+    "awayStep": 25,
+    "fontSize": 0
   },
   "level5": { 
     "opacity": .15,
-    "r": 2,
-    "chord": 5,
-    "awayStep": 5 
+    "r": 10,
+    "chord": 25,
+    "awayStep": 25,
+    "fontSize": 0
   },
   "default": { 
     "opacity": .1,
-    "r": 1,
-    "chord": 3,
-    "awayStep": 5 
+    "r": 10,
+    "chord": 30,
+    "awayStep": 50,
+    "fontSize": 0
   },
+  "defaultColor": "darkgray"
 }
+
+var colorScale = d3.scale.linear()
+    .range([ '#379154', '#39B4BF', '#FFE666', '#946FB0', '#E54E67' ]) // or use hex values
+    .domain([1980, 1990, 2000, 2010, 2020]);
+
 
 function calcCoords ( thisCenterX, thisCenterY, thisNumPoints, thisChord, thisAwayStep ) {
   
   var coords = [];    // output array
   var theta = thisChord / thisAwayStep;
-
-//  coords.push({x: thisCenterX, y: thisCenterY, level:level})
-
-//  console.log ("B: " + thisChord + ", " + thisAwayStep)
 
   for ( i = 0; i < thisNumPoints; i++ ) {
       var away = thisAwayStep * theta;
@@ -67,35 +75,6 @@ function calcCoords ( thisCenterX, thisCenterY, thisNumPoints, thisChord, thisAw
   }
 
   return coords;
-}
-
-function makeSpiral ( thisCenterX, thisCenterY, thisNumPoints, thisChord, thisAwayStep, level) {
-
-  new_time = calcCoords( thisCenterX, thisCenterY, thisNumPoints, thisChord, thisAwayStep, level );
-
-  var lineFunction = d3.svg.line()
-                      .x(function(d) { return d.x; })
-                      .y(function(d) { return d.y; })
-                      .interpolate("cardinal");
-
-  svg.append("path")
-    .attr("d", lineFunction(new_time))
-    .attr("stroke", "gray")
-    .attr("stroke-width", 0.5)
-    .attr("fill", "none");
-
-
-  var circles = svg.selectAll("svg")  // changed this selector to "svg" and it works, not sure why
-                  .data(new_time)
-                .enter()
-                  .append("circle")
-                  .attr("cx", function (d) { return d.x; })
-                  .attr("cy", function (d) { return d.y; })
-                  .attr("r", 5)
-                  .attr("class", function(d){ return "level".concat(d.level)});
-
-  return(new_time);
-
 }
 
 var lineFunction = d3.svg.line()
@@ -113,59 +92,67 @@ function plotSpiral ( root ) {
   svg.append("path")
     .attr("d", lineFunction(tempArray))
     .attr("stroke", "gray")
-    .attr("stroke-width", 1)
+    .attr("stroke-width", 5)
     .attr("fill", "none");
 
   var circles = svg.selectAll("svg")  // changed this selector to "svg" and it works, not sure why
-                  .data(root.children)
-                .enter()
-                  .append("circle")
-                  .attr("cx", function (d) { return d.x; })
-                  .attr("cy", function (d) { return d.y; })
-                  .attr("r", function (d) { 
-                    var r; 
-                    var level = "level".concat(d.level);
-                    //console.log("D: ", d.level );
-                    try{ r = spiralParams[level].r; }
-                    catch (err) { r = spiralParams["default"].r; }
-                    return r;
-                  })
-                  .style("opacity", function (d) {
-                    var opacity;
-                    var level = "level".concat(d.level);
-                    try{ opacity = spiralParams[level].opacity }
-                    catch (err) { opacity = spiralParams["default"].opacity }
-                    console.log("O: " + level + "," + opacity);
-                    return opacity;
-                  })
-                  .attr("class", function (d) { return "level".concat(d.level)});
-;
+        .data(root.children)
+      .enter()
+        .append("g")
+//        .attr("transform", function(d) { return "translate(" +d.x+"px, "+d.y + "px)"; })
+
+  circles.append("circle")
+        .attr("cx", function (d) { return d.x; })
+        .attr("cy", function (d) { return d.y; })
+        .attr("r", function (d) { 
+          var r; 
+          var level = "level".concat(d.level);
+          try{ r = spiralParams[level].r; }
+          catch (err) { r = spiralParams["default"].r; }
+          return r;
+        })
+        .style("opacity", function (d) {
+          var opacity;
+          var level = "level".concat(d.level);
+          try{ opacity = spiralParams[level].opacity }
+          catch (err) { opacity = spiralParams["default"].opacity }
+          return opacity;
+        })
+        .style("fill", function (d) {
+          var col;
+          var year = d.year;
+          if( year === undefined || year === null ) {
+            col = spiralParams["defaultColor"];
+          } else {
+            col = colorScale(year); 
+          }
+          if( col === "#NaNNaNNaN" ) { col = spiralParams["defaultColor"];}
+          console.log(year + " / " + col)
+          return col;
+        })
+        .attr("class", function (d) { return "level".concat(d.level)});
+
+  circles.append("text")
+  //  .attr("dy", ".31em")
+    .attr("text-anchor","middle")
+//    .style("transform", function(d) { return "translate("+d.x+"px, "+d.y+"px)"})
+    .attr("x", function(d){return d.x})
+    .attr("y", function(d){return d.y})
+    .attr("dy", "0.4em")
+    .attr("font-size",function(d) {
+      var fontSize;
+      var level = "level".concat(d.level);
+      try{ fontSize = spiralParams[level].fontSize }
+      catch (err) { fontSize = spiralParams["default"].fontSize }
+      return fontSize;
+    })
+    .attr("font-family","sans-serif")
+    .text(function(d) { return(d.name)});
 
   for( var i = 0; i < root.children.length; i++ ) {
     plotSpiral(root.children[i]);
   }
 
-}
-
-
-//makeSpiral ( centerX, centerY, numPoints, chord, awayStep );
-
-//makeSpiral ( new_time[199].x, new_time[199].y, 21, 10, 3 );
-
-function processPerson ( root, centerX, centerY, level ) {
-
-  console.log( "Processing " + root.name );
-
-  if( root.children.length == 0 ) { console.log("No children"); return; }
-
-  var coords = makeSpiral(centerX, centerY, root.children.length+1, chord, awayStep, level)
-  console.log( "COORDS.length: " + coords.length )
-  console.log( "CHILDREN.length: " + root.children.length )
-
-  for(var i=0; i<root.children.length; i++) {
-    console.log("CHILD: " + root.children[i].name)
-    processPerson( root.children[i], coords[i+1].x, coords[i+1].y, level+1)
-  }
 }
 
 function addCoords( root, centerX, centerY ) {
@@ -184,8 +171,6 @@ function addCoords( root, centerX, centerY ) {
     awayStep = spiralParams["default"].awayStep;
   }
 
-  console.log("PARAMS (" + level + "): " + chord + ", " + awayStep)
-
   var coords = calcCoords(centerX, centerY, root.children.length, chord, awayStep )
 
   for(var i = 0; i<root.children.length; i++ ) {
@@ -194,6 +179,8 @@ function addCoords( root, centerX, centerY ) {
 
   return(root)
 }
+
+/* *****  MAIN starts here ***** */
 
 var svg = d3.select("#chart").append("svg")
     .attr("width", width)
@@ -208,14 +195,13 @@ var centerX = width/2,   // x coordinate of spiral center
     awayStep = 10;       // rate of increase of spiral radius
 
 var z; 
-d3.json("output_GW.json", function(error, root) {
+d3.json("output_PGS.json", function(error, root) {
   if (error) throw error;
 
   root = addCoords( root, centerX, centerY );
   plotSpiral( root );
 
   z = root;   /// temporary so I can inspect it
-//  processPerson( root, centerX, centerY, root.level )
 });
 
 
